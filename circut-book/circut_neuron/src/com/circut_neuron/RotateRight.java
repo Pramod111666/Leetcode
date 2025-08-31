c5fe354f1600bc27bd325c62c68cd3c6ccb59692
pramodbhat111666 <pramodbhat111666@gmail.com>
2025-08-31 20:38:28 +0530

    feat: Removed unnecessary Eclipse metadata and added .gitignore

--- circut-book/circut_neuron/src/com/circut_neuron/RotateRight.java
+++
@@ -1,9 +0,0 @@
-package com.circut_neuron;
-
-public class RotateRight {
-
-	public static void main(String args[]) {
-		
-		System.out.print("Testing for the first commit");
-	}
-}
--- circut-book/circut_neuron/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module circut_neuron {
-}
